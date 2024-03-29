1781dd73ae81e51992a7a0f1aa2a21581269566a
sktm13 <77323556+sktm13@users.noreply.github.com>
2024-03-29 09:03:01 +0900

    Add files via upload
    생성

---
+++ etime/src/test/java/ejo/etime/EtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package ejo.etime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
