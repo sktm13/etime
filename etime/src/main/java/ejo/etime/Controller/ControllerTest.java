74f8c428bbedc3741aa5b6c02a9738f3b6ba890f
Jeongmin-Jeon <jjm4252@naver.com>
2024-04-04 20:20:41 +0900

    메인페이지에 index.html 연결
    ### 변경사항
    - home.html, index.html : 이름변경 home.html->index.html
    - ControllerTest.java : index.html 연결

--- etime/src/main/java/ejo/etime/Controller/ControllerTest.java
+++ etime/src/main/java/ejo/etime/Controller/ControllerTest.java
@@ -1,5 +1,10 @@
 package ejo.etime.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ControllerTest {
-    
+    @GetMapping("/")
+    String home(){
+        return "index.html";
+    }
 }
