47ef35f8e1e175b57ac931089f7113a55b97c319
sktm13 <77323556+sktm13@users.noreply.github.com>
2024-04-12 10:01:25 +0900

    Add files via upload

---
+++ etime/etime/src/main/java/ejo/etime/EtimeApplication.java
@@ -0,0 +1,14 @@
+package ejo.etime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtimeApplication.class, args);
+		// testtesttesttesttesttesttesttest
+	}
+
+}
