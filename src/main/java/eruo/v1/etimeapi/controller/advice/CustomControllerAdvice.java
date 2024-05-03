cd2cce539ec1288ceb52af6e5627caf20124522a
sktm13 <sktm13@naver.com>
2024-05-03 09:18:32 +0900

    코드 소실 커버

--- src/main/java/eruo/v1/etimeapi/controller/advice/CustomControllerAdvice.java
+++ src/main/java/eruo/v1/etimeapi/controller/advice/CustomControllerAdvice.java
@@ -9,17 +9,25 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import eruo.v1.etimeapi.util.CustomJWTException;
+
 //RestController의 예외를 처리하는 클래스
 @RestControllerAdvice
 public class CustomControllerAdvice {
-    
+
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<?> notExist(NoSuchElementException e){
+    public ResponseEntity<?> notExist(NoSuchElementException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("msg", e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> notExist(MethodArgumentNotValidException e){
+    public ResponseEntity<?> notExist(MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(Map.of("msg", e.getMessage()));
+    }
+
+    @ExceptionHandler(CustomJWTException.class)
+    protected ResponseEntity<?> handleJWTException(CustomJWTException e) {
+        String msg = e.getMessage();
+        return ResponseEntity.ok().body(Map.of("error", msg));
     }
 }
