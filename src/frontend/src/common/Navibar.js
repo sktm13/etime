0db3ab304d0bb8db00b675ff9c511bfc9f874237
PHS <skyito21@naver.com>
2024-06-06 23:05:01 +0900

    네비바 일부 수정

--- src/frontend/src/common/Navibar.js
+++ src/frontend/src/common/Navibar.js
@@ -3,17 +3,19 @@
 import React from 'react';
 import '../style/Navibar.css';
 
-import { Col, Row, Navbar, Form, Button, Dropdown, NavDropdown, Image} from 'react-bootstrap';
+import { Nav, Container, Col, Row, Navbar, Form, Button, Dropdown, DropdownButton, NavDropdown, Image} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom'
 
 import logoImage from '../style/image/sample_logo.png';
+import Avatar from '../style/image/Avatar.png';
+
 import { IoSearchOutline } from "react-icons/io5";
-import { PiUserCircleLight } from "react-icons/pi";
-import { RxHamburgerMenu } from "react-icons/rx";
+
 import { RxPencil2 } from "react-icons/rx";
 import { MdOutlinePayment } from "react-icons/md";
 import { BiDonateHeart } from "react-icons/bi";
-import { FaIdCardAlt } from "react-icons/fa";
+import { TbCategory } from "react-icons/tb";
+import { VscKebabVertical } from "react-icons/vsc";
 
 import { useState } from 'react';
 import {useCookies} from "react-cookie";
@@ -57,92 +59,70 @@ function Navibar() {
 
 
     return (
-        <Navbar className='navbar'>
-            <Navbar.Toggle />
-            <Navbar.Collapse className={"d-flex justify-content-center"}>
-                <Row className='col-md-12 col-xl-10 col-xxl-8 d-flex align-items-center navbar__list'>
-                    <Col className='d-flex justify-content-start d-flex align-items-center'>
-                        <Navbar.Brand href="/">
-                            <Image src={logoImage} className="logo-image" />
-                        </Navbar.Brand>
-                    </Col>
-                    <Col className='d-flex justify-content-start d-flex align-items-center'>
-                        <NavDropdown
-                            drop="end"
-                            style={{ color: 'black' }}
-                            className="nav-category-button"
-                            title="카테고리"
-                        >
-                            <div className="text-center">
-                                {categoryData.map((a, i) => (
-                                    <NavDropdown.Item key={i} onClick={() => dispatch(setCurrentCategory(i))}
-                                                      className="nav-category-Item">
-                                        {categoryData[i].name}
-                                    </NavDropdown.Item>
-                                ))}
-                            </div>
-                        </NavDropdown>
-                    </Col>
-                    <Col className='d-flex justify-content-center'>
-                        <Form.Group className="form mb-2 d-flex align-items-center" controlId="Form.SearchInput">
-                            <Form.Control size="sm" type="text" placeholder="관심있는 내용, 키워드 검색하기" className="search-control" />
-                            <Button variant="light" size="lg" type="submit" className="search-Button d-flex justify-content-center d-flex align-items-center">
+        <Navbar expand="lg">
+        <Container>
+            <Navbar.Brand href="/">
+                <Image src={logoImage} className="logo-image me-2" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto align-items-center justify-content-between nav-control">
+                    <NavDropdown
+                        drop="end"
+                        style={{ color: 'black' }}
+                        className="nav-category-button"
+                        title={<><TbCategory className="me-2" size={24} style={{ paddingBottom: '1px' }} />카테고리</>}
+                        
+                    >
+                        <div className="text-center">
+                            {categoryData.map((a, i) => (
+                                <NavDropdown.Item key={i} onClick={() => dispatch(setCurrentCategory(i))}
+                                                className="nav-category-Item">
+                                    {categoryData[i].name}
+                                </NavDropdown.Item>
+                            ))}
+                        </div>
+                    </NavDropdown>
+                
+                <Form.Group className="form me-2 mb-2 me-lg-3 d-flex align-items-center" controlId="Form.SearchInput">
+                            <Form.Control size="lg" type="text" placeholder="관심있는 내용, 키워드 검색하기" className="search-control" />
+                            <Button variant="light" size="lg" type="submit" 
+                                className="search-Button d-flex justify-content-center d-flex align-items-center">
                                 <IoSearchOutline style={{ color: '#212121' }}/>
                             </Button>
-                        </Form.Group>
-                    </Col>
-                    <Col className='d-flex justify-content-end'>
-                        <Col className='d-flex justify-content-end'>
-                            <Button variant="light" size="lg" action onClick={() => {isLogined?
-                                navigate('/pages/setting') : navigate('/pages/login')}}
-                                    className="profile-Button d-flex justify-content-center d-flex align-items-center">
-                                <PiUserCircleLight size="30" color='#343232'/>
+                </Form.Group>
+                
+                    <div className="d-flex">
+                            <Button variant="light" className="navi-Button me-1">
+                                <RxPencil2 size="20" color='#343232'/>
                             </Button>
-                        </Col>
-                        <Col className='nav-menu d-flex justify-content-start align-items-center'>
-                            <Dropdown>
-                                <Dropdown.Toggle className='nav-menu-button' variant="light" size="lg">
-                                    <RxHamburgerMenu size="23" color='#343232'/>
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu className='nav-menu-Item'>
-                                    <div className="text-center">
-                                        {isLogined === true ?
-                                            <Dropdown.Item variant="Dark" action onClick={() => {handleLogout()}}>로그아웃</Dropdown.Item> :
-                                            <Dropdown.Item variant="Dark" action onClick={() => {navigate('/pages/login')}}>로그인</Dropdown.Item>
-                                        }
-                                    </div>
-                                    <Dropdown.Divider />
-                                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                                        <Dropdown.Item variant="light" action onClick={() => {isLogined? navigate('/pages/CreatePost') : navigate('/pages/login')}}>
-                                            <RxPencil2 size="17" color='#343232'/>
-                                            <span style={{ marginLeft: '0.7rem' }}>글쓰기</span>
-                                        </Dropdown.Item>
-                                    </div>
-                                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                                        <Dropdown.Item variant="light" action onClick={() => {isLogined? navigate('/pages/donate') : navigate('/pages/login')}}>
-                                            <BiDonateHeart size="17" color='#343232'/>
-                                            <span style={{ marginLeft: '0.7rem' }}>후원</span>
-                                        </Dropdown.Item>
-                                    </div>
-                                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                                        <Dropdown.Item variant="light" action onClick={() => {isLogined? navigate('/pages/payment') : navigate('/pages/login')}}>
-                                            <MdOutlinePayment size="17" color='#343232'/>
-                                            <span style={{ marginLeft: '0.7rem' }}>결제</span>
-                                        </Dropdown.Item>
-                                    </div>
-                                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                                        <Dropdown.Item variant="light" action onClick={() => {isLogined? navigate('/pages/license') : navigate('/pages/login')}}>
-                                            <FaIdCardAlt size="17" color='#343232'/>
-                                            <span style={{ marginLeft: '0.7rem' }}>전문가 신청</span>
-                                        </Dropdown.Item>
-                                    </div>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Col>
-                    </Col>
-                </Row>
+                            <Button variant="light" className="navi-Button me-1">
+                                <BiDonateHeart size="20" color='#343232'/>
+                            </Button>
+                            <Button variant="light" className="navi-Button me-1">
+                                <MdOutlinePayment size="20" color='#343232'/>
+                            </Button>
+                    </div>
+                        <Button variant="light" className="profile-Button me-1"
+                                onClick={() => {isLogined ? navigate('/pages/setting') : navigate('/pages/login')}}>
+                        <Image src={Avatar} style={{ width: '32px', height: '32px' }} alt="Profile" />
+                        </Button>
+
+                        <DropdownButton variant="outline-light" className="nav-menu-button" id="dropdown-basic-button" 
+                            size='sm' title={<VscKebabVertical color='black' />} drop="end">
+                            {
+                                isLogined === true ?
+                                    <Dropdown.Item className="nav-menu-Item" onClick={() => {handleLogout()}}>로그아웃</Dropdown.Item> :
+                                    <Dropdown.Item className="nav-menu-Item" onClick={() => {navigate('/pages/login')}}>로그인</Dropdown.Item>
+                            }
+                            <Dropdown.Item className="nav-menu-Item" onClick={() => {navigate('/pages/setting')}}>설정</Dropdown.Item>
+                            <Dropdown.Item className="nav-menu-Item" onClick={() => {navigate('/pages/license')}}>전문가 신청</Dropdown.Item>
+                        </DropdownButton>                     
+                </Nav>
             </Navbar.Collapse>
-        </Navbar>
+            
+        </Container>
+    </Navbar>
         // <Container fluid className={"navbar__container"}>
         //     <Navbar>
         //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
